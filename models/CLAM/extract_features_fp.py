23a3e3e9693ea3d13a346f8fac1259e8cb410c1d
Christophoros-Spyretos <chrsp39@liu.se>
2024-09-13 10:34:27 +0200

    configure them to multirun

--- models/CLAM/extract_features_fp.py
+++ models/CLAM/extract_features_fp.py
@@ -6,6 +6,7 @@ from functools import partial
 import torch
 import torch.nn as nn
 import timm
+import glob
 from torch.utils.data import DataLoader
 from PIL import Image
 import h5py
@@ -29,7 +30,8 @@ def seed_torch(seed=7):
     torch.manual_seed(seed)
     if device.type == 'cuda':
         torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+		# if you are using multi-GPU.
+        torch.cuda.manual_seed_all(seed) 
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
@@ -59,31 +61,37 @@ def compute_w_loader(output_path, loader, model, verbose = 0):
 	
 	return output_path
 
-def build_experiment_name(cfg):
-    return '_'.join([str(cfg.seed),
-					 cfg.data_h5_dir, 
-					 cfg.data_slide_dir, 
-					 cfg.slide_ext, 
-					 cfg.csv_path, 
-                     cfg.feat_dir, 
-					 cfg.model_name, 
-					 str(cfg.batch_size), 
-					 str(cfg.num_workers),
-                     str(cfg.no_auto_skip), 
-					 str(cfg.target_patch_size)])
+def build_experiment_name(cfg):	
+	return '_'.join([str(cfg.seed),
+					cfg.data_h5_dir, 
+					cfg.data_slide_dir, 
+					cfg.slide_ext, 
+					cfg.csv_path, 
+					cfg.feat_dir, 
+					cfg.model_name, 
+					str(cfg.batch_size), 
+					str(cfg.num_workers),
+					str(cfg.no_auto_skip), 
+					str(cfg.target_patch_size)])
 
 @hydra.main(version_base="1.3.2", 
 			config_path= '/home/chrsp39/CBTN_Histology_Multi_Modal/configs/pre_processing', 
-			config_name='extract_features_fp')
-
+			config_name= 'extract_features_fp')
+			
 def main(cfg:DictConfig):
-	# seed everything
-	seed_torch(cfg.seed)
+	for data_h5_dir, data_slide_dir, csv_path, feat_dir in zip(cfg.data_h5_dirs, cfg.data_slide_dirs, cfg.csv_paths, cfg.feat_dirs):
+		# Update the configuration for each run
+		with open_dict(cfg):
+			cfg.data_h5_dir = data_h5_dir
+			cfg.data_slide_dir = data_slide_dir
+			cfg.csv_path = csv_path
+			cfg.feat_dir = feat_dir
 
-    # get experiment name 
-	experiment_name = build_experiment_name(cfg)
+		# seed everything
+		seed_torch(cfg.seed)
+		print(f'Processing {cfg.data_h5_dir} with slide directory {cfg.data_slide_dir}')
 	
-	settings ={'seed' : cfg.seed,
+		settings ={'seed' : cfg.seed,
 			'data_h5_dir' : cfg.data_h5_dir,
 			'data_slide_dir' : cfg.data_slide_dir,
 			'slide_ext' : cfg.slide_ext,
@@ -95,62 +103,64 @@ def main(cfg:DictConfig):
 			'no_auto_skip' : cfg.no_auto_skip,
 			'target_patch_size' : cfg.target_patch_size}
 
+		# get experiment name 
+		print(cfg)
+		experiment_name = build_experiment_name(cfg)
 
-	print('initializing dataset')
-	csv_path = cfg.csv_path
-	if csv_path is None:
-		raise NotImplementedError("CSV path must be provided in the configuration.")
+		print('initializing dataset')
+		csv_path = cfg.csv_path
+		if csv_path is None:
+			raise NotImplementedError("CSV path must be provided in the configuration.")
 
-	bags_dataset = Dataset_All_Bags(csv_path)
-	
-	os.makedirs(cfg.feat_dir, exist_ok=True)
-	os.makedirs(os.path.join(cfg.feat_dir, 'pt_files'), exist_ok=True)
-	os.makedirs(os.path.join(cfg.feat_dir, 'h5_files'), exist_ok=True)
-	dest_files = os.listdir(os.path.join(cfg.feat_dir, 'pt_files'))
+		bags_dataset = Dataset_All_Bags(csv_path)
+		
+		os.makedirs(cfg.feat_dir, exist_ok=True)
+		os.makedirs(os.path.join(cfg.feat_dir, 'pt_files'), exist_ok=True)
+		os.makedirs(os.path.join(cfg.feat_dir, 'h5_files'), exist_ok=True)
+		dest_files = os.listdir(os.path.join(cfg.feat_dir, 'pt_files'))
 
-	model, img_transforms = get_encoder(cfg.model_name, target_img_size=cfg.target_patch_size)
-			
-	_ = model.eval()
-	model = model.to(device)
-	total = len(bags_dataset)
+		model, img_transforms = get_encoder(cfg.model_name, target_img_size=cfg.target_patch_size)
+				
+		_ = model.eval()
+		model = model.to(device)
+		total = len(bags_dataset)
 
-	loader_kwargs = {'num_workers': cfg.num_workers, 'pin_memory': True} if device.type == "cuda" else {}
+		loader_kwargs = {'num_workers': cfg.num_workers, 'pin_memory': True} if device.type == "cuda" else {}
 
-	for bag_candidate_idx in tqdm(range(total)):
-		slide_id = bags_dataset[bag_candidate_idx].split(cfg.slide_ext)[0]
-		bag_name = slide_id+'.h5'
-		h5_file_path = os.path.join(cfg.data_h5_dir, 'patches', bag_name)
-		slide_file_path = os.path.join(cfg.data_slide_dir, slide_id+cfg.slide_ext)
-		print('\nprogress: {}/{}'.format(bag_candidate_idx+1, total))
-		print(slide_id)
+		for bag_candidate_idx in tqdm(range(total)):
+			slide_id = bags_dataset[bag_candidate_idx].split(cfg.slide_ext)[0]
+			bag_name = slide_id+'.h5'
+			h5_file_path = os.path.join(cfg.data_h5_dir, 'patches', bag_name)
+			slide_file_path = os.path.join(cfg.data_slide_dir, slide_id+cfg.slide_ext)
+			print('\nprogress: {}/{}'.format(bag_candidate_idx+1, total))
+			print(slide_id)
 
-		if not cfg.no_auto_skip and slide_id+'.pt' in dest_files:
-			print('skipped {}'.format(slide_id))
-			continue 
+			if not cfg.no_auto_skip and slide_id+'.pt' in dest_files:
+				print('skipped {}'.format(slide_id))
+				continue 
 
-		output_path = os.path.join(cfg.feat_dir, 'h5_files', bag_name)
-		time_start = time.time()
-		wsi = openslide.open_slide(slide_file_path)
-		dataset = Whole_Slide_Bag_FP(file_path=h5_file_path, 
-									wsi=wsi, 
-									img_transforms=img_transforms)
+			output_path = os.path.join(cfg.feat_dir, 'h5_files', bag_name)
+			time_start = time.time()
+			wsi = openslide.open_slide(slide_file_path)
+			dataset = Whole_Slide_Bag_FP(file_path=h5_file_path, 
+										wsi=wsi, 
+										img_transforms=img_transforms)
 
-		loader = DataLoader(dataset=dataset, batch_size=cfg.batch_size, **loader_kwargs)
-		output_file_path = compute_w_loader(output_path, loader = loader, model = model, verbose = 1)
+			loader = DataLoader(dataset=dataset, batch_size=cfg.batch_size, **loader_kwargs)
+			output_file_path = compute_w_loader(output_path, loader = loader, model = model, verbose = 1)
 
-		time_elapsed = time.time() - time_start
-		print('\ncomputing features for {} took {} s'.format(output_file_path, time_elapsed))
+			time_elapsed = time.time() - time_start
+			print('\ncomputing features for {} took {} s'.format(output_file_path, time_elapsed))
 
-		with h5py.File(output_file_path, "r") as file:
-			features = file['features'][:]
-			print('features size: ', features.shape)
-			print('coordinates size: ', file['coords'].shape)
+			with h5py.File(output_file_path, "r") as file:
+				features = file['features'][:]
+				print('features size: ', features.shape)
+				print('coordinates size: ', file['coords'].shape)
 
-		features = torch.from_numpy(features)
-		bag_base, _ = os.path.splitext(bag_name)
-		torch.save(features, os.path.join(cfg.feat_dir, 'pt_files', bag_base+'.pt'))
+			features = torch.from_numpy(features)
+			bag_base, _ = os.path.splitext(bag_name)
+			torch.save(features, os.path.join(cfg.feat_dir, 'pt_files', bag_base+'.pt'))
 
 if __name__ == '__main__':
 	main()
 	print("finished!")
-
