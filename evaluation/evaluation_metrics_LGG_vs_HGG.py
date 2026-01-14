7aadcb667a9ffefd7032d5d64eb45fbdcd1f810a
Christophoros-Spyretos <chrsp39@liu.se>
2026-01-14 09:39:08 +0100

    new results

--- evaluation/evaluation_metrics_LGG_vs_HGG.py
+++ evaluation/evaluation_metrics_LGG_vs_HGG.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # %% LOAD RESULTS
 # path to results
-results_path = '/local/data1/chrsp39/CBTN_Histology_Multi_Stain/models/CLAM/eval_results/EVAL_LGG_vs_HGG_KI67_small_clam_sb_conch_v1_mixup'
+results_path = '/local/data1/chrsp39/CBTN_Histology_Multi_Stain/models/CLAM/eval_results/LGG_vs_HGG/EVAL_LGG_vs_HGG_Early_Fusion_KI67_only___abmil_resnet50'
 contents = os.listdir(results_path)
 
 folds_dict = {} 
@@ -61,10 +61,10 @@ summary = pd.DataFrame(columns=[
 for fold in folds:
     new_row = pd.DataFrame({
         'Task': ['LGG_vs_HGG'],
-        'Modality': ['KI67'],
-        'Feature_Encoder': ['conch_v1'], 
-        'Aggregation': ['small_clam_sb'],
-        'Fusion': ['Single_Stain_gaussian_noise'],
+        'Modality': ['KI67_only'],
+        'Feature_Encoder': ['resnet50'], 
+        'Aggregation': ['abmil'],
+        'Fusion': ['Early_Fusion'],
         'Fold': [str(fold)],
         'BA': [0],
         'MCC': [0],
@@ -75,7 +75,7 @@ for fold in folds:
 
 # save the summary dataframe to a csv file
 save_path = '/local/data1/chrsp39/CBTN_Histology_Multi_Stain/evaluation/LGG_vs_HGG'
-save_name = 'EVAL_LGG_vs_HGG_KI67_small_clam_sb_conch_v1_mixup.csv'
+save_name = 'EVAL_LGG_vs_HGG_Early_Fusion_KI67_only___abmil_resnet50.csv'
 summary.to_csv(os.path.join(save_path, save_name), index=False)
 
 
